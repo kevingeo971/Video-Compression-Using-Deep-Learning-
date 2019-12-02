7b3854c2521452badd6bc380369b8d4b1cb001e6
Ali Lakdawala <amlk@gatech.edu>
2019-12-02 17:42:30 -0500

    main with arg parsers

--- Eva_Compression/Main.py
+++ Eva_Compression/Main.py
@@ -1,47 +1,42 @@
 
 # imports
-import cv2 
-import matplotlib.pyplot as plt
-import numpy as np
+import cv2
 import pickle
-import torch
-import torch.utils.data as utils
-from sklearn.model_selection import train_test_split
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-from sklearn.preprocessing import scale
-import torch
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torch.utils.data import sampler
-import torchvision.datasets as dset
-import torchvision.transforms as T
+import os
+import time
+import imageio
 import numpy as np
 from tqdm import tqdm
-import pandas as pd
-
-
-%load_ext autoreload
-%autoreload 2
-
-%pylab inline
-
-import os
-import cv2
-import time
-
+import argparse
+from glob import glob
+import matplotlib.pyplot as plt
 import torch
-import torchvision as tv
-import torchvision.transforms as transforms
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision as tv
+import torch.optim as optim
+import torch.utils.data as utils
 from torch.autograd import Variable
-from torchvision.utils import save_image
-import imageio
+import torch.nn.functional as F
+import torchvision.transforms as T
 import torchvision.datasets as dset
-
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+from sklearn.preprocessing import scale
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.model_selection import train_test_split
 
 
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+
+
+print(args)
