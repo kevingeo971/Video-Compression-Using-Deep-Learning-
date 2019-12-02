4ccba712a3b6112089a08c066b77fbbe8c5d811e
mikegroff <mike.groff@gatech.edu>
2019-12-02 18:04:36 -0500

    data loader and main

--- Eva_Compression/Main.py
+++ Eva_Compression/Main.py
@@ -26,6 +26,10 @@ import torchvision.transforms as transforms
 from sklearn.preprocessing import scale
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.model_selection import train_test_split
+from CAE import CAE
+from cluster import ClusterModule
+from load_data import load_data
+from train import train
 
 
 
@@ -40,3 +44,38 @@ args = parser.parse_args()
 
 
 print(args)
+
+
+path = ''
+train = False
+
+if train:
+    pass
+else:
+    test_loader = load_data(path)
+    model_n = CAE()
+    model_n.load_state_dict(torch.load("CAE_Full_data.pwf"))
+    
+    enc_frames = np.zeros((1,1250))
+
+    for batch_idx, batch in enumerate(test_loader):
+        
+        output = model_n(batch[0].cuda(),encode=True)
+        enc_frames = np.vstack((enc_frames, output.detach().cpu().numpy()))
+    enc_frames = enc_frames[1:,:]  
+
+    CM = ClusterModule()
+    labels = CM.run(enc_frames)                             
+
+    clusters_seen = []
+    index_list = []
+    for i in range(enc_frames.shape[0]):
+        clust = labels[i]
+        if clust not in clusters_seen:
+            clusters_seen.append(clust)
+            index_list.append(i)
+
+    print(index_list)
+
+
+    
