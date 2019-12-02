9d05b207a9a175869e7f0b82399b5a9264da2bc7
Ali Lakdawala <amlk@gatech.edu>
2019-12-02 17:12:52 -0500

    uploading cluster.py

---
+++ Eva_Compression/cluster.py
@@ -0,0 +1,27 @@
+import time
+from sklearn.cluster import AgglomerativeClustering
+
+
+# Assume you have compressed images: images_compressed
+# original_images -> images_compressed (output of the encoder network)
+
+class ClusterModule:
+
+    def __init__(self):
+        self.ac = None
+
+    def run(self, image_compressed, fps=10):
+        n_samples = image_compressed.shape[0]
+        self.ac = AgglomerativeClustering(n_clusters=n_samples // fps)
+        start_time = time.time()
+        self.ac.fit(image_compressed)
+        print("Time to fit ", n_samples, ": ", time.time() - start_time)
+        return self.ac.labels_
+
+    def plot_distribution(self):
+        import matplotlib.pyplot as plt
+        fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
+        axs.hist(self.ac.labels_, bins=max(self.ac.labels_) + 1)
+        plt.xlabel("Cluster Numbers")
+        plt.ylabel("Number of datapoints")
+
