49f7054790cdb97d7afe78c27fabeb8b357d86b6
mikegroff <mike.groff@gatech.edu>
2019-12-04 19:44:39 -0500

    new file

--- Eva_Compression/preprocessing.py
+++ Eva_Compression/preprocessing.py
@@ -1,4 +1,19 @@
+import cv2
 
-def generate_frames(path=''):
+def write_frames(path_from, path_to):
 
-    
+    cap = cv2.VideoCapture(path_from)
+
+    while(cap.isOpened()):
+        frameId = int(cap.get(1)) #current frame number
+        framename = str(frameId)
+        ret, frame = cap.read()
+        pre = 10-len(framename)
+        if (ret != True):
+            break
+        #storing frames in new folder 
+        filename =path_to + '_' + pre*'0' + framename
+        cv2.imwrite(filename, frame)
+    cap.release()
+
+    return
